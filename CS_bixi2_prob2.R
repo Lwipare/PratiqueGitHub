8460a18f770b43926487dbbbb112e1ac874c1136
Louis Paré <louis.pare.2@ulaval.ca>
2021-11-04 20:46:27 -0400

    Chargement version fonctionnelle

--- CS_bixi2_prob2.R
+++ CS_bixi2_prob2.R
@@ -2,6 +2,6 @@ setwd("~/Documents/coursift1902/2018")
 
  station <- as.data.frame(read.csv("./Stations_2018.csv", fileEncoding = "UTF-8")[, c(1,2)])
 
-station$code[grep("Rachel", station[, 2])]
+code <- station$code[grep("Rachel", station[, 2])]
 
 
