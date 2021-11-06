c18758b8b3a57dec6754dab54f0f3594691d83a3
Louis Paré <louis.pare.2@ulaval.ca>
2021-11-05 22:01:43 -0400

    Remplacement du > par >= et idem pour < par <=

--- CS_bixi2_prob3.R
+++ CS_bixi2_prob3.R
@@ -1,10 +1,10 @@
 
 
 # retrait des dates antérieures
-databixi.f <- databixi.f[as.Date(databixi.f$start_date) > as.Date("2018-05-02"), ]
+databixi.f <- databixi.f[as.Date(databixi.f$start_date) >= as.Date("2018-05-02"), ]
 
 # retrait des dates postérieures
-databixi.f <- databixi.f[as.Date(databixi.f$end_date) < as.Date("2018-08-02"), ]
+databixi.f <- databixi.f[as.Date(databixi.f$end_date) <= as.Date("2018-08-02"), ]
 
 # sélection des stations pertinents
 
