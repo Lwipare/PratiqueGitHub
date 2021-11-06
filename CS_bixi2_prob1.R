747d62419ea588e67a59680d62a5f7b458aff7f2
Louis Paré <louis.pare.2@ulaval.ca>
2021-11-05 21:55:55 -0400

    Version optimisée avec fonction import

--- CS_bixi2_prob1.R
+++ CS_bixi2_prob1.R
@@ -1,18 +1,26 @@
 setwd("~/Documents/coursift1902/2018")
 
-# initialisation avec les données de mai 2018
-databixi.f <- read.csv(
-    "OD_2018-05.csv",
-    fileEncoding = "UTF-8",
-    colClasses = c(
-        start_date = "Date",
-        start_station_code = "factor" ,
-        end_date = "Date",
-        end_station_code = "factor",
-        is_member = "factor",
-        duration_sec = "numeric"
+
+# définition de la fonction d'importation
+import <- function(nom)
+{
+    databixi.f <- read.csv(
+        nom,
+        fileEncoding = "UTF-8",
+        colClasses = c(
+            start_date = "Date",
+            start_station_code = "factor" ,
+            end_date = "Date",
+            end_station_code = "factor",
+            is_member = "factor",
+            duration_sec = "numeric"
+        )
     )
-)
+}
+
+# initialisation avec les données de mai 2018
+databixi.f <- import("OD_2018-05.csv")
+
 
 # Définition de la liste des dates
 dates <- as.Date(c("2018-06-22", "2018-07-22", "2018-08-22"))
@@ -22,20 +30,10 @@ for (i in seq_along(dates))
 {
     moisc <- substring(as.character(dates[i]), 6, 7)
     nom <- paste0("OD_2018-", moisc, ".csv")
-    print(c("processing", nom))
-    databixi <-
-        read.csv(
-            nom,
-            fileEncoding = "UTF-8",
-            colClasses = c(
-                start_date = "Date",
-                start_station_code = "factor" ,
-                end_date = "Date",
-                end_station_code = "factor",
-                is_member = "factor",
-                duration_sec = "numeric"
-            )
-        )
+    print(paste("Processing", nom))
+    databixi <- import(nom)
+
 
     databixi.f <- rbind(databixi.f,databixi)
 }
+
