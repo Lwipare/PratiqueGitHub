f43674f2e3bea0b272af0f5cfa25207d3859e720
Louis Paré <lpare2000@yahoo.com>
2022-02-05 09:58:19 -0500

    Suppression d'un fichier inutile

--- CS_bixi2_prob2.R
+++
@@ -1,7 +0,0 @@
-setwd("~/Documents/coursift1902/2018")
-
- station <- as.data.frame(read.csv("./Stations_2018.csv", fileEncoding = "UTF-8")[, c(1,2)])
-
-code <- station$code[grep("Rachel", station[, 2])]
-
-
